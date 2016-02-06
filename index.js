efa7c65f3e94e4e867b00b62f7dc7336866f2c28
Laryea Quaye <laryea.quaye@blueacorn.com>
2016-02-06 15:50:45 -0500

    improved whitelist, validated command line arguments, fixed paths for target and origin when inspecting files

--- index.js
+++ index.js
@@ -4,53 +4,64 @@ var lwip = require('lwip');
 
 var pathToMedia = process.argv[2];
 var pathToCopyMedia = process.argv[3];
+
+if (!pathToMedia || !pathToCopyMedia) {
+  console.error("Must provide arguments for existing media directory and target location.");
+}
+
 var sampleSize = 10;
 var images = {};
-var extensions = [/.*\.png/i,/.*\jpg/i];
+var extensions = [
+  /.*\.png/i,
+  /.*\.jpg/i,
+  /.*\.jpeg/i,
+  /.*\.gif/i
+];
 
-console.log(pathToCopyMedia);
 if (!fs.existsSync(pathToCopyMedia)){
   fs.mkdirSync(pathToCopyMedia); 
 }
 
-var globOptions = {
-  'nodir': true
-};
-
-glob(pathToMedia + '/**/*', globOptions, function(err, files) {
-  var processed = 0;
+glob(pathToMedia + '/**/*', function(err, files) {
   files.forEach(function(file) {
-    //todo create white list of image files
-    if(extensions.every(function(regex){return !file.match(regex);})) {
-      fs.createWriteStream(pathToCopyMedia + '/' + file);
-      return;
-    }
-    lwip.open(file, function(err, image) {
-      if (err) {
-        console.error(err)
-        fs.createWriteStream(pathToCopyMedia + '/' + file);
+    var pathToCopyFile = pathToCopyMedia + file.replace(pathToMedia, '');
+    if (fs.lstatSync(file).isDirectory() && !fs.existsSync(pathToCopyFile)) {
+      fs.mkdirSync(pathToCopyFile);
+    } else {
+      //todo create white list of image files
+      if(extensions.every(function(regex){return !file.match(regex);})) {
+        fs.createWriteStream(pathToCopyFile);
         return;
       }
+      lwip.open(file, function(err, image) {
+        if (err) {
 
-      var width = image.width(),
-      height = image.height();
-      if (!images.hasOwnProperty(width)) {
-        images[width] = {};
-      }
-      if (!images[width].hasOwnProperty(height)) {
-        images[width][height] = [];
-      }
+          console.error(err)
+          fs.createWriteStream(pathToCopyFile);
+          return;
+        }
 
-      if (images[width][height].length < sampleSize) {
-        images[width][height].push(file);
-        // Create new image file in new folder
-        fs.createWriteStream(pathToCopyMedia + '/' + file);
-      } else {
-        // Create symlink in new folder
-        var randomImage = images[width][height][Math.floor(images[width][height].length * Math.random)];
-        fs.symlink(pathToCopyMedia + '/' + randomImage, pathToCopyMedia + '/' + file);
+        var width = image.width(),
+          height = image.height();
+        if (!images.hasOwnProperty(width)) {
+          images[width] = {};
+        }
+        if (!images[width].hasOwnProperty(height)) {
+          images[width][height] = [];
+        }
 
-      }
-    });
+
+        if (images[width][height].length < sampleSize) {
+          images[width][height].push(file);
+          // Create new image file in new folder
+          fs.createWriteStream(pathToCopyFile);
+        } else {
+          // Create symlink in new folder
+          var randomIndex = Math.floor(images[width][height].length * Math.random());
+          var randomImage = images[width][height][randomIndex];
+          fs.symlink(pathToCopyMedia + randomImage.replace(pathToMedia, ''), pathToCopyFile);
+        }
+      });
+    }
   });
 });
